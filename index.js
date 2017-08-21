64e12644e3820d87dfa119d6a775ed023ccb51d7
Ashok Koduru <ashok.koduru9@gmail.com>
2017-08-21 13:24:59 -0400

    Adding privacy policy

--- index.js
+++ index.js
@@ -13,12 +13,17 @@ app.use(bodyParser.urlencoded({
 }));
 
 // Process application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //Initial route
 app.get('/', function(req, res){
 	res.send("Hello world, I am a chatbot - Ashok")
-})
+});
+
+// Privacy policy - facebook is Happy :)
+app.get('/privacy', function(req, res){
+    res.send('<h2 style="padding:20px;text-decoration:underline">At your Service</h2><p style="font-size: 18px; padding: 10px 30px">This application is built only for learning purposes and is not intended for any kind of commercial activity. We do not collect any kind of user\'s personal information at all. We honor user\'s privacy and do not track anything at all. It is not developed in order to attract anyone under 13.</p>')
+});
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
@@ -26,10 +31,10 @@ app.get('/webhook/', function (req, res) {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
-})
+});
 
 
 // Spin up the server
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
-})
+});

21a4e9a5e88d1175a44d7e9be4b760cf63a62ea5
Ashok Koduru <ashok.koduru9@gmail.com>
2017-09-06 14:00:00 +0100

    Adding Web hook

--- index.js
+++ index.js
@@ -42,13 +42,83 @@ app.post('/webhook/', function (req, res) {
     for (let i = 0; i < messaging_events.length; i++) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
+        console.log('Sender ID:' + sender);
         if (event.message && event.message.text) {
-            let text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            let text = event.message.text.toLowerCase();
+            console.log('Msg text: "' + text + '"');
+
+            if ((text.indexOf('#plot') === 0 && text.indexOf('#plot ') !== 0) ||
+                (text.indexOf('#suggest') === 0 && text.indexOf('#suggest ') !== 0) ||
+                (text.indexOf('#starring') === 0 && text.indexOf('#starring ') !== 0) ||
+                (text.indexOf('#meta') === 0 && text.indexOf('#meta ') !== 0)) {
+                sendTextMessage(sender, "Something went wrong. You mistyped something it looks like!")
+            } else if (text.indexOf('#plot ') === 0) {
+                sendMoviePlot(sender, text.substring(text.indexOf(' ') + 1));
+            } else if (text.indexOf('#suggest ') === 0) {
+                sendMovieSuggestions(sender, text.substring(text.indexOf(' ') + 1));
+            } else if (text.indexOf('#starring ') === 0) {
+                sendPersonMoviesData(sender, text.substring(text.indexOf(' ') + 1));
+            } else if (text.indexOf('#meta ') === 0) {
+                sendMovieMetadata(sender, text.substring(text.indexOf(' ') + 1));
+            } else {
+                sendTextMessage(sender, changeTextNatural(text.substring(0, 319)));
+            }
+        } else if (event.postback && event.postback.payload) {
+            sendTextMessage(sender, changeTextNatural(event.postback.payload));
         }
     }
-    res.sendStatus(200)
+    res.sendStatus(200);
 });
+
+
+function sendMovieMetadata(sender, text) {
+    var options = {
+        method: 'GET',
+        url: 'http://api.themoviedb.org/3/search/movie',
+        qs: {
+            query: String(text),
+            include_adult: true,
+            language: 'en',
+            api_key: TMDb_API_KEY
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log(response);
+            throw new Error(error);
+        }
+        var jsonbody = JSON.parse(body);
+        if (jsonbody.total_results > 0) {
+            let moviedata = jsonbody.results[0]
+            let response = moviedata.title + "\n";
+
+            if (moviedata.vote_average) {
+                response += "Vote Average: " + moviedata.vote_average + " / 10\n";
+            }
+
+            if (moviedata.genre_ids && moviedata.genre_ids.length) {
+                let genre_names = genres.reduce(function (arr, obj) {
+                    if (moviedata.genre_ids.indexOf(obj.id) > -1) {
+                        arr.push(obj.name)
+                    }
+                    return arr;
+                }, [])
+                if (genre_names) {
+                    response += "Genre: " + genre_names.join(", ") + "\n"
+                }
+            }
+
+            if (moviedata.release_date) {
+                response += "Release Date: " + dateFormat(new Date(moviedata.release_date), "dddd, mmmm dS, yyyy")
+            }
+            sendTrailerButtonWithTextAndPoster(sender, moviedata, response);
+        } else {
+            sendTextMessage(sender, "Duh! Nothing found..");
+        }
+    });
+
+}
 
 
 // Spin up the server
